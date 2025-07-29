e532ee7459c947b1e43f0477ad6e1a381633ec51
shriya <shriyarai42@gmail.com>
2025-07-29 13:55:20 +0530

    Added _redirects file for routing

--- book-frontend/src/components/Login.js
+++ book-frontend/src/components/Login.js
@@ -51,7 +51,7 @@ function Login() {
     e.preventDefault();
 
     try {
-      const response = await axios.post('https://book-backend-bmpr.onrender.com/api/auth/login', {
+      const response = await axios.post('https://book-backend-bmpr.onrender.com/api/auth/login', {
         username,
         password,
       });
